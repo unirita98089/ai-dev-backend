e54af9996de43313fc7ec0624499a19dba66a809
masaki_sudo <masaki_sudo@unirita.co.jp>
2023-07-12 17:29:56 +0900

    todoの一覧取得実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -16,7 +16,7 @@ class TodoController extends Controller
 
     public function index()
     {
-        return $this->todoRepository->all();
+        return response()->json($this->todoRepository->all());
     }
 
 }
